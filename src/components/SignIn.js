92b1a9a5d1e8d85b0704362e54883e7f97ab68d8
ugcmedia <ugcmedia@gmail.com>
2020-07-19 15:26:50 +0500

    Sign in made responsive

---
+++ src/components/SignIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import "../css/SignInAndSignUp.css";
+import SignInForm from "./SignInForm";
+
+const SignIn = () => {
+  return (
+    <div className="sign-in-up-page">
+      <SignInForm />
+    </div>
+  );
+};
+
+export default SignIn;
