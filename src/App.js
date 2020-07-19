21cd8e7526fac89f725308d8f5d6d5c0a8c6b302
ugcmedia <ugcmedia@gmail.com>
2020-07-19 16:10:53 +0500

    Added all routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./App.scss";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import PreHome from "./components/PreHome";
+// import Home from "./components/Home";
+// import Iphone from "./components/Iphone";
+// import Macbook from "./components/Macbook";
+
+function isAuth() {
+  if (!localStorage.getItem("apple-token")) return false;
+  else return true;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-primary">Button</button>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route
+            exact
+            path="/prehome"
+            render={() =>
+              isAuth() ? <PreHome /> : <Redirect to="/?error403" />
+            }
+          />
+          {/* <Route
+            exact
+            path="/home"
+            render={() => (isAuth() ? <Home /> : <Redirect to="/?error403" />)}
+          />
+          <Route
+            exact
+            path="/iphone"
+            render={() =>
+              isAuth() ? <Iphone /> : <Redirect to="/?error403" />
+            }
+          />
+          <Route
+            exact
+            path="/macbook"
+            render={() =>
+              isAuth() ? <Macbook /> : <Redirect to="/?error403" />
+            }
+          /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
