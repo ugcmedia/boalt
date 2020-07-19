573b8eed346b9797d9892c6adeed2c9fe1fbce65
ugcmedia <ugcmedia@gmail.com>
2020-07-19 15:28:19 +0500

    Sign up content

---
+++ src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/SignInAndSignUp.css";
+import SignUpForm from "./SignUpForm";
+
+const SignUp = () => {
+  return (
+    <div className="sign-in-up-page">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUp;
