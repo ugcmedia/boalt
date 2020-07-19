ad6c12884edab623366c53a15439e5aeaa1e91d4
ugcmedia <ugcmedia@gmail.com>
2020-07-20 04:40:29 +0500

    content added

--- src/components/Iphone.js
+++ src/components/Iphone.js
@@ -31,7 +31,58 @@ class Iphone extends React.Component {
       <div>
         <Navbar />
           <Container >
-            
+            <div>
+              <Row>
+                <Col lg="6">
+                  {/* <div className="iphone-title-part">
+                    <span className="iphone-title">
+                    <p >iPhone</p>
+                    </span>
+                    <span className="iphone-title-sub">
+                    <p >The Ultimate iPhone</p>
+                    </span>
+                    <span className="iphone-title-description">
+                    <p > The future is here. Join the iPhone Upgrade Program to get the
+                      latest iPhone - NOW!</p>
+                    </span>
+                  </div> */}
+                </Col>
+                {/* <Col lg="6">
+                <div className="iphone-image-part">
+                  <img src={iphone_image} alt="iPhone" className="iphone-image" />
+                </div>
+                </Col> */}
+              </Row>
+            </div>
+            {/* <div>
+              <Row>
+                <Col lg="8">
+                  <div className="bottom-part">
+                    <div>
+                      <p className="price-tag">From $699</p>
+                      <p className="buy-now">Buy Now</p>
+                    </div>
+                    <div className="switch-part">
+                      <img
+                        onClick={this.clickedFront}
+                        src={iPhoneFrontThumbnail}
+                        alt="iPhone Front"
+                        className="iphone-front"
+                      />
+                      <img
+                        onClick={this.clickedBack}
+                        src={iPhoneRareThumbnail}
+                        alt="iPhone Rare"
+                        className="iphone-rare"
+                      />
+                      <div className="toggle-line" onClick={this.handle}>
+                        <div className={btn_class} onClick={this.handle}></div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row> 
+            </div>*/}
         </Container>
       </div>
     );
