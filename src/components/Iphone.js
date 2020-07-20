d0ee3e7d9ca846100c9675a030dbb614e1bc44df
ugcmedia <ugcmedia@gmail.com>
2020-07-21 01:05:33 +0500

    axios requets

--- src/components/Iphone.js
+++ src/components/Iphone.js
@@ -46,14 +46,7 @@ class Iphone extends React.Component {
   };
 
   componentDidMount(){
-    axios.get("https://boalt-interview.herokuapp.com/api/shipping-dates",
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      }
-  }).then(res=> console.log(res))
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res=> console.log(res))
   }
 
   render() {
@@ -84,7 +77,7 @@ class Iphone extends React.Component {
     <g id="Group_4" data-name="Group 4" transform="translate(386.5 522.376)" opacity="0.18">
       <g id="Group_1" data-name="Group 1" transform="translate(0)">
         <path id="Path_1" data-name="Path 1" d="M306.051,135.481c13.346-17.59,31.9-17.676,31.9-17.676s2.759,16.537-10.5,32.468C313.3,167.285,297.205,164.5,297.205,164.5S294.184,151.122,306.051,135.481Z" transform="translate(-214.687 -117.805)" fill="#ccc" fillRule="evenodd"/>
-        <path id="Path_2" data-name="Path 2" d="M134.809,273.621c6.866,0,19.608-9.438,36.194-9.438,28.55,0,39.782,20.315,39.782,20.315a44.107,44.107,0,0,0-21.967,38.483c0,30.743,27.365,41.338,27.365,41.338s-19.129,53.842-44.967,53.842c-11.868,0-21.093-8-33.6-8-12.742,0-25.388,8.3-33.623,8.3-23.594,0-53.4-51.074-53.4-92.128,0-40.392,25.23-61.582,48.895-61.582C114.873,264.749,126.811,273.621,134.809,273.621Z" transform="translate(-50.594 -215.34)" fill="#ccc" fill-Rule="evenodd"/>
+        <path id="Path_2" data-name="Path 2" d="M134.809,273.621c6.866,0,19.608-9.438,36.194-9.438,28.55,0,39.782,20.315,39.782,20.315a44.107,44.107,0,0,0-21.967,38.483c0,30.743,27.365,41.338,27.365,41.338s-19.129,53.842-44.967,53.842c-11.868,0-21.093-8-33.6-8-12.742,0-25.388,8.3-33.623,8.3-23.594,0-53.4-51.074-53.4-92.128,0-40.392,25.23-61.582,48.895-61.582C114.873,264.749,126.811,273.621,134.809,273.621Z" transform="translate(-50.594 -215.34)" fill="#ccc" fillRule="evenodd"/>
       </g>
     </g>
   </g>
