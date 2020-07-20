d4ad202be6c2a4ea3c71120d6c2933d04dd40d84
ugcmedia <ugcmedia@gmail.com>
2020-07-20 17:01:39 +0500

    nav clean up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
-import logo from "../images/AppleLogo.png";
 
 const NavLinks = (props) => {
   if (props.small) {
